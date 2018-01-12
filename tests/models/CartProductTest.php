8c5e53add4dae025586ffa43455713a2cd085893
Tobias Kündig <tobias@offline.swiss>
2018-01-12 16:47:53 +0100

    Testing a sqlite only setup

--- tests/models/CartProductTest.php
+++ tests/models/CartProductTest.php
@@ -1,14 +1,10 @@
 <?php namespace OFFLINE\Mall\Tests\Models;
 
-use OFFLINE\Mall\Models\CustomField;
-use OFFLINE\Mall\Models\Product;
-use OFFLINE\Mall\Models\Variant;
 use PluginTestCase;
 
 class CartProductTest extends PluginTestCase
 {
     public function test_()
     {
-
     }
 }
