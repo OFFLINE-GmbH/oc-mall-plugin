5824cd8b0ef8ffdceae8d6e91ecafdd71d51f7aa
Tobias Kündig <tobias@offline.swiss>
2018-01-16 17:16:55 +0100

    Added RangeFilter Test

--- classes/categoryfilter/Filter.php
+++ classes/categoryfilter/Filter.php
@@ -3,7 +3,44 @@
 namespace OFFLINE\Mall\Classes\CategoryFilter;
 
 
-class Filter
-{
+use Illuminate\Support\Collection;
+use OFFLINE\Mall\Models\Property;
 
+abstract class Filter
+{
+    public $property;
+    public $specialProperties = ['price'];
+
+    public function __construct($property)
+    {
+        if ($property instanceof Property) {
+            $property = $property->id;
+        }
+
+        $this->property = $property;
+    }
+
+    public function setFilterValues(Collection $items): Collection
+    {
+        return $items->map(function ($item) {
+            $item->setAttribute('filter_value', $this->getFilterValue($item));
+
+            return $item;
+        })->reject(function ($item) {
+            return $item->filter_value === null;
+        });
+    }
+
+    public function getFilterValue($item)
+    {
+        if (\in_array($this->property, $this->specialProperties, true)) {
+            return $item->getOriginal($this->property);
+        }
+
+        $value = $item->property_values->where('property_id', $this->property)->first();
+
+        return $value ? $value->value : null;
+    }
+
+    abstract public function apply(Collection $items): Collection;
 }
--- classes/categoryfilter/RangeFilter.php
+++ classes/categoryfilter/RangeFilter.php
@@ -3,7 +3,25 @@
 namespace OFFLINE\Mall\Classes\CategoryFilter;
 
 
-class RangeFilter
-{
+use Illuminate\Support\Collection;
 
+class RangeFilter extends Filter
+{
+    public $minValue;
+    public $maxValue;
+
+    public function __construct($property, $minValue, $maxValue)
+    {
+        parent::__construct($property);
+        $this->minValue = $minValue;
+        $this->maxValue = $maxValue;
+    }
+
+    public function apply(Collection $items): Collection
+    {
+        return $this->setFilterValues($items)->filter(function ($item) {
+            return $item->filter_value >= $this->minValue
+                && $item->filter_value <= $this->maxValue;
+        });
+    }
 }
