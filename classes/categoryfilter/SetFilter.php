903363fbbbaa317deaf8a5a56a17addc2bc8f79c
Tobias Kündig <tobias@offline.swiss>
2018-01-18 14:26:19 +0100

    Implemented SetFilter

---
+++ classes/categoryfilter/SetFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OFFLINE\Mall\Classes\CategoryFilter;
+
+
+use Illuminate\Support\Collection;
+
+class SetFilter extends Filter
+{
+    public $values;
+
+    public function __construct($property, array $values)
+    {
+        parent::__construct($property);
+        $this->values = $values;
+    }
+
+    public function apply(Collection $items): Collection
+    {
+        return $this->setFilterValues($items)->filter(function ($item) {
+            return in_array($item->filter_value, $this->values);
+        });
+    }
+}
