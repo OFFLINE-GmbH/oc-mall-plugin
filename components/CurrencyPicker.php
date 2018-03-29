e33679e39de3dae811e923f9027f277d53eb5491
Tobias Kündig <tobias@offline.swiss>
2018-03-29 10:04:58 +0200

    Imported CurrencyPicker component

---
+++ components/CurrencyPicker.php
@@ -0,0 +1,67 @@
+<?php namespace OFFLINE\Mall\Components;
+
+use Cms\Classes\ComponentBase;
+use OFFLINE\Mall\Classes\Traits\SetVars;
+use OFFLINE\Mall\Models\CurrencySettings;
+use October\Rain\Router\Router as RainRouter;
+use Redirect;
+
+class CurrencyPicker extends ComponentBase
+{
+    use SetVars;
+
+    public $currencies;
+    public $activeCurrency;
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'offline.mall::lang.components.currencyPicker.details.name',
+            'description' => 'offline.mall::lang.components.currencyPicker.details.description',
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+    public function onRun()
+    {
+        $this->setVar('currencies', CurrencySettings::get('currencies'));
+        $this->setVar('activeCurrency', CurrencySettings::activeCurrency());
+    }
+
+    public function onSwitchCurrency()
+    {
+        if ( ! $currency = post('currency')) {
+            return;
+        }
+
+        CurrencySettings::setActiveCurrency($currency);
+
+        $pageUrl = $this->getUrl();
+
+        // preserve the query string, if it exists
+        $query   = http_build_query(request()->query());
+        $pageUrl = $query ? $pageUrl . '?' . $query : $pageUrl;
+
+        return Redirect::to($pageUrl);
+    }
+
+    protected function getUrl()
+    {
+        $page = $this->getPage();
+
+        if (isset($page->apiBag['staticPage'])) {
+            $staticPage = $page->apiBag['staticPage'];
+            $localeUrl = array_get($staticPage->attributes, 'viewBag.url');
+        } else {
+            $router = new RainRouter;
+            $params = $this->getRouter()->getParameters();
+            $localeUrl = $router->urlFromPattern($page->url, $params);
+        }
+
+        return $localeUrl;
+    }
+}
