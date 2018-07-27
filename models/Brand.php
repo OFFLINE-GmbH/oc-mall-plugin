2c55638db3a6c48e4e75654f613ad5f10b9e8b2b
Tobias Kündig <tobias@offline.swiss>
2018-07-27 09:58:17 +0200

    Added slug column to brands

--- models/Brand.php
+++ models/Brand.php
@@ -1,6 +1,7 @@
 <?php namespace OFFLINE\Mall\Models;
 
 use Model;
+use October\Rain\Database\Traits\Sluggable;
 use October\Rain\Database\Traits\Sortable;
 use October\Rain\Database\Traits\Validation;
 use System\Models\File;
@@ -9,13 +10,16 @@ class Brand extends Model
 {
     use Validation;
     use Sortable;
+    use Sluggable;
 
     public $implement = ['@RainLab.Translate.Behaviors.TranslatableModel'];
     public $translatable = [
         'name',
         'website',
     ];
-
+    public $slugs = [
+        'slug' => 'name',
+    ];
     public $rules = [
         'name'    => 'required',
         'website' => 'url',
