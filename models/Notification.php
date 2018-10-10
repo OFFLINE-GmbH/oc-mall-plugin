3904c88ae3612a8ad59e7a56a68ed86311169613
Tobias Kündig <tobias@offline.swiss>
2018-10-10 10:30:22 +0200

    Fixed table seeder

--- models/Notification.php
+++ models/Notification.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Cache;
 use Model;
 use October\Rain\Database\Traits\Sortable;
 use October\Rain\Database\Traits\Validation;
+use October\Rain\Support\Facades\Schema;
 
 class Notification extends Model
 {
@@ -30,6 +31,10 @@ class Notification extends Model
 
     public static function getEnabled()
     {
+        if ( ! Schema::hasTable('offline_mall_notifications')) {
+            return collect([]);
+        }
+
         return Cache::rememberForever(self::CACHE_KEY, function () {
             return Notification::where('enabled', true)->get()->pluck('template', 'code');
         });
