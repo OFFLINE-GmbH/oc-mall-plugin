4f10633a5039b67795e77632a3e86815bbb67dc3
Tobias Kündig <tobias@offline.swiss>
2018-10-21 11:50:13 +0200

    Documented Checkout component

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,6 +34,7 @@ module.exports = {
                     '/components/products',
                     '/components/cart',
                     '/components/discount-applier',
+                    '/components/checkout',
                     '/components/address-form',
                     '/components/address-list',
                     '/components/address-selector',
