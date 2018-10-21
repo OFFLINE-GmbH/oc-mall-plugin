64022dede06e44fe7b3aafa702beb01473fd884a
Tobias Kündig <tobias@offline.swiss>
2018-10-21 12:10:33 +0200

    Refactored OrdersList component

--- components/OrdersList.php
+++ components/OrdersList.php
@@ -1,15 +1,39 @@
 <?php namespace OFFLINE\Mall\Components;
 
 use Auth;
+use Illuminate\Support\Collection;
 use OFFLINE\Mall\Models\GeneralSettings;
 use OFFLINE\Mall\Models\Order;
 
+/**
+ * The OrdersList component displays a list of all the user's orders.
+ */
 class OrdersList extends MallComponent
 {
-    public $orders = [];
-    public $countries = [];
+    /**
+     * Array of all orders.
+     *
+     * @var Collection
+     */
+    public $orders;
+    /**
+     * All available countries.
+     *
+     * @var Collection
+     */
+    public $countries;
+    /**
+     * Link to pay a pending order.
+     *
+     * @var string
+     */
     public $paymentLink;
 
+    /**
+     * Component details.
+     *
+     * @return array
+     */
     public function componentDetails()
     {
         return [
@@ -18,11 +42,21 @@ class OrdersList extends MallComponent
         ];
     }
 
+    /**
+     * Properties of this component.
+     *
+     * @return array
+     */
     public function defineProperties()
     {
         return [];
     }
 
+    /**
+     * The component is initialized.
+     *
+     * @return void
+     */
     public function init()
     {
         $user = Auth::getUser();
@@ -30,14 +64,19 @@ class OrdersList extends MallComponent
             return;
         }
 
-        $this->orders = Order::byCustomer($user->customer)
-                             ->with(['products', 'products.variant'])
-                             ->orderBy('created_at', 'DESC')
-                             ->get();
-
         $this->paymentLink = $this->getPaymentLink();
+        $this->orders = Order
+            ::byCustomer($user->customer)
+            ->with(['products', 'products.variant'])
+            ->orderBy('created_at', 'DESC')
+            ->get();
     }
 
+    /**
+     * Get the URL of the payment page.
+     *
+     * @return string
+     */
     protected function getPaymentLink()
     {
         $page = GeneralSettings::get('checkout_page');
