cc897fe6a9dc344a02fe667301d51715117beb29
Tobias Kündig <tobias@offline.swiss>
2018-12-21 16:59:46 +0100

    Added Going Live Checklist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,6 +25,7 @@ module.exports = {
                     '/digging-deeper/shipping-methods',
                     '/digging-deeper/payments',
                     '/digging-deeper/taxes',
+                    '/digging-deeper/going-live',
                 ]
             },
             {
