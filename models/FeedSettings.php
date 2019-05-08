d9aafd6976e4559944e673802673b51c3da64783
Tobias Kündig <tobias@offline.swiss>
2019-05-08 17:36:59 +0200

    Generate Feed key on filterFields

--- models/FeedSettings.php
+++ models/FeedSettings.php
@@ -11,7 +11,7 @@ class FeedSettings extends Model
     public $settingsCode = 'offline_mall_settings';
     public $settingsFields = '$/offline/mall/models/settings/fields_feeds.yaml';
 
-    public function onRefresh()
+    public function filterFields()
     {
         if (FeedSettings::get('google_merchant_key') === null) {
             FeedSettings::set('google_merchant_key', str_random(12));
