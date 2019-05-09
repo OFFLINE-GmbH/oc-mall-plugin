336ac1c67babd2a0e368d2f2452ee90896d2ffc9
Tobias Kündig <tobias@offline.swiss>
2019-05-09 09:14:53 +0200

    Fixed index table migration

--- updates/add_name_column_to_index_table.php
+++ updates/add_name_column_to_index_table.php
@@ -8,6 +8,9 @@ class AddNameColumnToIndexTable extends Migration
 {
     public function up()
     {
+        if ( ! Schema::hasTable('offline_mall_index')) {
+            return;
+        }
         Schema::table('offline_mall_index', function (Blueprint $table) {
             if ( ! Schema::hasColumn('offline_mall_index', 'name')) {
                 $table->string('name');
