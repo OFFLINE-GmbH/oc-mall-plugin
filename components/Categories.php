e7976e7b8d4bd377c58348351aa5890c8a5472d2
Alain Martini <webmaster@inalto.com>
2019-11-28 12:03:48 +0100

    Categories component created

---
+++ components/Categories.php
@@ -0,0 +1,19 @@
+<?php namespace Offline\Mall\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Categories extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Categories Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
