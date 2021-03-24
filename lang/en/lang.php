ed42801dd7b0e36ba26ac665c3c07ada8b940b52
stnKrisna <krisnarocks@users.noreply.github.com>
2021-03-25 02:04:00 +1100

    Added missing comma

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -517,7 +517,7 @@ return [
             'expired' => 'This promo code has expired.',
             'not_found' => 'This promo code is not valid.',
             'usage_limit_reached' => 'This promo code has been applied to many times and is therefore no longer valid.',
-            'cart_limit_reached' => 'Promo code limit reached. You can no longer add promo codes for this cart.'
+            'cart_limit_reached' => 'Promo code limit reached. You can no longer add promo codes for this cart.',
         ],
     ],
     'payment_method' => [
