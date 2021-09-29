1de17f68d5e055b4342e8bb8e5f9df061611674d
Alex <55833027+PubliAlex@users.noreply.github.com>
2021-09-29 13:42:18 +0200

    Create create_offline_mall_discounts_products.php
    Create table for the relation between discounts and products

---
+++ updates/create_offline_mall_discounts_products.php
@@ -0,0 +1,21 @@
+<?php namespace OFFLINE\Mall\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateOfflineMallDiscountsProducts extends Migration
+{
+    public function up()
+    {
+        Schema::create('offline_mall_discounts_products', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->integer('discount_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('offline_mall_discounts_products');
+    }
+}
