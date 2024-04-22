e9334f5c20c7e81298cd31e8f79d213db937a5aa
Sam <sam@rat.md>
2024-04-23 01:43:19 +0200

    [FIX] lastFile error

--- controllers/Products.php
+++ controllers/Products.php
@@ -127,12 +127,12 @@ class Products extends Controller
         // Strike through all old file versions.
         Event::listen('backend.list.injectRowClass', function ($lists, $record) {
             $latestFile = $this->vars['formModel']->latest_file;
-            
-            if (empty(trim($latestFile->version))) {
-                return '';
-            }
 
             if (!$latestFile || !$record instanceof ProductFile) {
+                return '';
+            }
+            
+            if (empty(trim($latestFile->version))) {
                 return '';
             }
 
