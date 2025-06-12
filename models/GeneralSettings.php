a8fb98048bee7d41d65d8bcc72eba8388b7663f0
Damien MATHIEU <contact@hounddd.fr>
2025-06-12 16:39:26 +0200

    [FIX] return cms pages with checkout and quickCheckout

--- models/GeneralSettings.php
+++ models/GeneralSettings.php
@@ -19,13 +19,19 @@ class GeneralSettings extends Model
         Cache::forget('offline_mall.mysql.index.driver');
     }
 
-    public function getPagesByComponent($component)
+    /**
+     * Get Pages by CMS Component
+     * @param string $component
+     * @param bool $allOnEmpty
+     * @return array
+     */
+    public function getPagesByComponent(string $component, bool $allOnEmpty = true)
     {
         $theme = Theme::getActiveTheme();
         $pages = Page::listInTheme($theme, true);
-        
+
         $cmsPages = [];
-        
+
         foreach ($pages as $page) {
             if (!$page->hasComponent($component)) {
                 continue;
@@ -33,11 +39,17 @@ class GeneralSettings extends Model
             $cmsPages[$page->baseFileName] = $page->title;
         }
 
-        return count($cmsPages) < 1
-            ? $this->allPages()
-            : $cmsPages;
+        if (count($cmsPages) < 1) {
+            return $allOnEmpty ? $this->allPages() : [];
+        } else {
+            return $cmsPages;
+        }
     }
 
+    /**
+     * Return all CMS Pages
+     * @return array
+     */
     protected function allPages()
     {
         return Page
@@ -48,35 +60,62 @@ class GeneralSettings extends Model
             ->toArray();
     }
 
+    /**
+     * Return CMS Pages with [product] component
+     * @return array
+     */
     public function getProductPageOptions()
     {
         return $this->getPagesByComponent('product');
     }
 
+    /**
+     * Return CMS Pages with [products] component
+     * @return array
+     */
     public function getCategoryPageOptions()
     {
         return $this->getPagesByComponent('products');
     }
 
+    /**
+     * Return CMS Pages with [addressForm] component
+     * @return array
+     */
     public function getAddressPageOptions()
     {
         return $this->getPagesByComponent('addressForm');
     }
 
+    /**
+     * Return CMS Pages with [checkout] and [quickCheckout] component
+     * @return array
+     */
     public function getCheckoutPageOptions()
     {
-        return $this->getPagesByComponent('checkout');
+        $result = array_merge(
+            $this->getPagesByComponent('checkout', false),
+            $this->getPagesByComponent('quickCheckout', false),
+        );
+
+        return empty($result) ? $this->allPages() : $result;
     }
 
+    /**
+     * Return CMS Pages with [myAccount] component
+     * @return array
+     */
     public function getAccountPageOptions()
     {
         return $this->getPagesByComponent('myAccount');
     }
 
+    /**
+     * Return CMS Pages with [cart] component
+     * @return array
+     */
     public function getCartPageOptions()
     {
         return $this->getPagesByComponent('cart');
     }
-
-
 }
